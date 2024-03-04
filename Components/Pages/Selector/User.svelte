caa7fad15e735ab85c2a9d2cb127bb45784a5ce2
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-03-04 09:21:54 +0100

    Add v6 flag

--- Components/Pages/Selector/User.svelte
+++ Components/Pages/Selector/User.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import ProfilePicture from "$lib/Components/ProfilePicture.svelte";
   import { Login } from "$state/Login/ts/main";
+  import { UpdateIcon } from "$ts/images/general";
   import { getProfilePicture } from "$ts/stores/pfp";
   import { UserData } from "$types/user";
   import { onMount } from "svelte";
@@ -24,6 +25,11 @@
 {#if data.acc.enabled}
   <button class="user" on:click={go}>
     <ProfilePicture src={pfp} height={50} />
-    <p class="username">{name}</p>
+    <p class="username">
+      <span>{name} </span>
+      {#if data.acc.v6}
+        <img src={UpdateIcon} alt="" class="v6" title="{name} is a v6 user!" />
+      {/if}
+    </p>
   </button>
 {/if}
